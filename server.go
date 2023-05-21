93b9921db04c7c714c8bcf26f8e3b0907dccc69f
Tanay Nagar <swathi.a@signdesk.com>
2023-05-21 15:36:09 +0530

    added create and find functionalities

--- server.go
+++ server.go
@@ -11,6 +11,16 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+	app.Get("/create", func(c *fiber.Ctx) error {
+		config.CreateUser("Rohit")
+		return c.SendString("create!")
+	})
+
+	app.Get("/find", func(c *fiber.Ctx) error {
+		config.FindUser()
+		return c.SendString("find!")
+	})
+	
 	config.InitMongo()
 	app.Listen(":3000")
 }
